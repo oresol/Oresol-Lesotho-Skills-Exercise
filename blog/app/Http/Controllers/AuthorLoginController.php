d62c2f4b1a94549f38c1435cec9ea96fa7eda640
MMolieleng <nosichefane@gmail.com>
2023-10-29 16:09:37 +0200

    modified login controller

--- blog/app/Http/Controllers/AuthorLoginController.php
+++ blog/app/Http/Controllers/AuthorLoginController.php
@@ -8,14 +8,21 @@ use Illuminate\Support\Facades\Auth; // Import the Auth facade
 class AuthorLoginController extends Controller
 {
     public function LoginAuthor(Request $request) {
-        $credentials = $request->only('email', 'password');
-
-        if (Auth::attempt($credentials)) {
-           
-            return view('add_article');
+        $email = $request->input('email');
+        $password = $request->input('password');
+    
+        // Find the user by email (assuming email is unique)
+        $user = Author::where('email', $email)->first();
+    
+        if (!$user) {
+            return response()->json(['status' => 400, 'message' => 'Login Fail, please check your email']);
+        }
+    
+        if ($user->password == $password) {
+            // Password matches, authentication is successful
+            return view('about_us');
         } else {
-           
-            return response()->json(['status' => 400, 'message' => 'Login Fail, please check your credentials']);
+            return response()->json(['status' => 400, 'message' => 'Login Fail, please check your password']);
         }
     }
-}
+}    
